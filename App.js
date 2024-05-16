8a6c37ba1328652241533f33989247d849f49c54
AngieNoemiCV <20460159@colima.tecnm.mx>
2024-05-16 14:34:57 -0600

    Pantallas

--- App.js
+++ App.js
@@ -1,20 +1,66 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { NavigationContainer } from '@react-navigation/native';
+import { Icon } from 'react-native-elements';
+
+import HealthScreen from './screens/HealthScreen';
+import SportsScreen from './screens/SportsScreen';
+import TechScreen from './screens/TechScreen';
+import Business from './screens/Bussines';
+import Foro from './screens/Foro';
+import Perfil from './screens/Perfil';
+
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator>
+
+        <Tab.Screen name="Inicio" component={Business}
+          options={{
+            tabBarIcon: (props) => (
+              <Icon type='entypo' name='home' color={props.color} />
+            ),
+          }} />
+
+        <Tab.Screen name="Estadisticas" component={HealthScreen}
+          options={{
+            tabBarIcon: (props) => (
+              <Icon type='materialicons' name='bar-chart' color={props.color} />
+            ),
+          }} />
+
+        <Tab.Screen name="Trofeos" component={SportsScreen}
+          options={{
+            tabBarIcon: (props) => (
+              <Icon type='entypo' name="trophy" color={props.color} />
+            ),
+          }} />
+
+        <Tab.Screen name="Asesorias" component={TechScreen}
+          options={{
+            tabBarIcon: (props) => (
+              <Icon type='entypo' name="mail" color={props.color} />
+            ),
+          }} />
+
+        <Tab.Screen name="Foro" component={Foro}
+          options={{
+            tabBarIcon: (props) => (
+              <Icon type='materialicons' name='forum' color={props.color} />
+            ),
+          }} />
+
+        <Tab.Screen name="Perfil" component={Perfil}
+          options={{
+            tabBarIcon: (props) => (
+              <Icon type='Ionicins' name='people' color={props.color} />
+            ),
+          }} />
+
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ screens/TechScreen.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import {View, Text, StyleSheet, Image} from 'react-native';
+
+
+
+
+const TechScreen = ({navigation}) => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.texto}>
+        Todavía no hay ningún recordatorio. ¡Pulsa "+" para agregar uno!
+      </Text>
+    </View>
+  );
+};
+
+
+
+export default TechScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#8fcbbc',
+  },
+  imagen: {
+    width: 100,
+    height: 100,
+  },
+  texto: {
+    fontSize: 18,
+    textAlign: 'center',
+    marginTop: 10,
+    marginBottom: 10,
+    marginLeft: 30,
+    marginRight: 30,
+  },
+});
